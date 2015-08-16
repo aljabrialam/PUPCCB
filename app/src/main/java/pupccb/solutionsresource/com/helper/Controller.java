3ab6509b5ba98206e6931a51a058c289d575c6c0
Aljabri Alam <aljabrialam@gmail.com>
2015-08-16 23:41:18 +0800

    -Rework Login and Ticket UI -Prepare backend for Login and Registration

--- app/src/main/java/pupccb/solutionsresource/com/helper/Controller.java
+++ app/src/main/java/pupccb/solutionsresource/com/helper/Controller.java
@@ -22,21 +22,6 @@ public class Controller {
     }
     public enum MethodTypes{
         LOGIN, REGISTER
-
-    public void setError(ErrorHandler.Error error, MethodTypes methodTypes) {
-        if (error.getErrorMessage().contains("java.io.EOFException")) {
-
-        } else if (error.getErrorType().equals(ErrorHandler.ErrorType.LOGOUT)) {
-
-        } else {
-            if (activity instanceof Main) {
-                ((Main) activity).setError(error, methodTypes);
-            }
-        }
-    }
-
-    public enum MethodTypes {
-        LOGIN
     }
 
     private Activity activity;
@@ -52,6 +37,18 @@ public class Controller {
     public void loginResult(Session session, Login login){
         if(activity instanceof Main){
             ((Main) activity).loginResult(session, login);
+        }
+    }
+
+    public void register(Activity activity,RegistrationDetails registrationDetails){
+        this.activity = activity;
+        this.registrationDetails = registrationDetails;
+        onlineCommunicator.register(this, activity, registrationDetails, MethodTypes.REGISTER);
+    }
+
+    public void registerResult(RegistrationResponse registrationResponse, RegistrationDetails registrationDetails){
+        if(activity instanceof Registration){
+            ((Registration) activity).registerResult(registrationResponse, registrationDetails);
         }
     }
 }
